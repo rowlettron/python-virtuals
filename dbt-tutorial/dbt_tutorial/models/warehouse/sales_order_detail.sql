0652c1e69fd89ec024150784c998e6f9168fc4dd
rowlettron <rowlettron@yahoo.com>
2024-10-11 12:27:47 -0500

    Created new file for star schema

--- dbt-tutorial/dbt_tutorial/models/warehouse/sales_order_detail.sql
+++ dbt-tutorial/dbt_tutorial/models/warehouse/sales_order_detail.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='ephemeral') }}
+
 SELECT salesorderid,
        salesorderdetailid,
        carriertrackingnumber,
--- dbt-tutorial/dbt_tutorial/models/warehouse/sales_order_header.sql
+++ dbt-tutorial/dbt_tutorial/models/warehouse/sales_order_header.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='ephemeral') }}
+
 SELECT salesorderid,
        revisionnumber,
        orderdate,
